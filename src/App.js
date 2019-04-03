119c54d46c5a9020bd94c54de3dc1b866b5c4886
Uros <uros.zivkovic96@gmail.com>
2019-04-03 17:54:09 +0200

    feat(*): add rate and ethToUsd services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css'
 import Web3 from 'web3'
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
+import getExchangeRate from './services/getExchangeRate.service.js';
+import { get } from 'http';
+
+console.log(getExchangeRate())
+
 
 class App extends Component {
     render() {
